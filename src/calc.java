fd64771684c24e8c4a2bebaa95c45a1ee6f34ea6
Hecton <hectonpdomingos@gmail.com>
2016-11-14 22:59:14 -0200

    Testing intelliJ IDEA

---
+++ src/calc.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String [] args){
+
+    Double priceUSPS[] = {0.0,1.1,2.3,3.3};
+
+    System.out.println("Weight LBS");
+        Scanner scan = new Scanner(System.in);
+       int i =  scan.nextInt();
+
+System.out.println(priceUSPS[i]);
+    }
+}
